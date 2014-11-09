f3a14afadc4cf5c797828200b90e36ad4f8ac37e
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-11-09 18:17:55 +0000

    Added class for boids and a method

--- boid_class.py
+++ boid_class.py
@@ -16,13 +16,48 @@ class boid(object):
 	   self.yposition=yposition
 	   self.xvelocity=xvelocity
 	   self.yvelocity=yvelocity
+"""
+           
+boids=[boid(random.uniform(*boids_starting_x_position_range),random.uniform(*boids_starting_y_position_range),random.uniform(*boids_starting_x_velocity_range),random.uniform(*boids_starting_y_position_range)) for i in range(number_of_boids)]   
+
+for boid in boids:
+   print boid.xvelocity  
+
+"""
+
+class boids(object):
+   def __init__(self,number_of_boids):
+      self.number_of_boids=number_of_boids
+      self.boids=[boid(random.uniform(*boids_starting_x_position_range),random.uniform(*boids_starting_y_position_range),random.uniform(*boids_starting_x_velocity_range),random.uniform(*boids_starting_y_position_range)) for i in range(number_of_boids)] 
+   
+   def velocity_change(self,threshold):
+	   for i in self.boids:
+		for j in self.boids:
+	            i.boid.xvelocity=i.boid.xvelocity+(j.boid.xposition-i.boid.xposition)*threshold
+	   return boid.xvelocity
+"""
+class boids(object):
+   def __init__(self,number_of_boids):
+      self.number_of_boids=number_of_boids
+      boids=[boid(random.uniform(*boids_starting_x_position_range),random.uniform(*boids_starting_y_position_range),random.uniform(*boids_starting_x_velocity_range),random.uniform(*boids_starting_y_position_range)) for i in range(self.number_of_boids)]   
 	   
-	
-		
-		
+   def velocity_change(self,threshold):
+	   for i in self.boids:
+		for j in self.boids:
+	            i.boid.xvelocity=i.boid.xvelocity+(j.boid.xposition-i.boid.xposition)*threshold
+	   return boid.xvelocity
 		
 
-boids=[boid(random.uniform(*boids_starting_x_position_range),random.uniform(*boids_starting_y_position_range),
-random.uniform(*boids_starting_x_velocity_range),random.uniform(*boids_starting_y_velocity_range)) for i in range(number_of_boids)]
-	   
+		
+
+
+
+print boid.xvelocity
+
+
+
+# boid in boids:
+  # print boid.velocity_change(0.01*number_of_boids)
+
+""" 
 
