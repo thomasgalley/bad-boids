7cce2cfa73e7ed6538fa19f29247909698364ba9
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-11-11 12:06:48 +0000

    Cleaned up class, added a random boid method

--- parameters.py
+++ parameters.py
@@ -1,3 +1,5 @@
+import random
+
 number_of_boids= 50
 
 boids_starting_x_position_range= [-450,50.0]
@@ -8,3 +10,8 @@ attractionstrength=0.01
 boidproximitythreshold=100
 matchspeed_distance=10000
 matchspeed_strength=0.125
+
+boids_initial_x_positions=[random.uniform(*boids_starting_x_position_range) for x in range(number_of_boids)]
+boids_initial_y_positions=[random.uniform(*boids_starting_y_position_range) for x in range(number_of_boids)]
+boid_initial_x_velocities=[random.uniform(*boids_starting_x_velocity_range) for x in range(number_of_boids)]
+boid_initial_y_velocities=[random.uniform(*boids_starting_y_velocity_range) for x in range(number_of_boids)]
