15ea8b17c6bd43e4d3911ae4b42936201105d85b
thomasgalley <thomas.galley.14@ucl.ac.uk>
2014-11-11 16:50:12 +0000

    Changed fly_away_from_boids function to a method

--- boid_class.py
+++ boid_class.py
@@ -36,12 +36,24 @@ class boids(object):
 
        def initial_flock(self,number_of_boids):
            
-           self.boids=([boid(0,0,0,0).random_boid() for i in range (number_of_boids)])
+           self.boids=[boid(0,0,0,0).random_boid() for i in range (number_of_boids)]
 
        def fly_to_centre(self,number_of_boids):
            for boid in self.boids:
              for member in self.boids:
                 boid.xvelocity=velocity_change(boid.xvelocity,boid.xposition,member.xposition,self.attraction_strength/number_of_boids)
+                boid.yvelocity=velocity_change(boid.yvelocity,boid.yposition,member.yposition,self.attraction_strength/number_of_boids)
+
+       def fly_away_from_boids(self,number_of_boids):
+        for boid in self.boids:
+           for member in self.boids:
+                        if position_difference_test(boid.xposition,member.xposition,boid.yposition,member.yposition,self.boidproximitythreshold):
+				boid.xvelocity=velocity_change(boid.xvelocity,boid.xposition,member.xposition,1)
+				boid.yvelocity=velocity_change(boid.yvelocity,boid.yposition,member.yposition,1)
+
+
+
+
 
 
 
